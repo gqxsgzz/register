9fe3903641271f4ddaa011a63bb2a9f21be7f799
gqxsgzz <975240116@qq.com>
2020-03-22 21:20:33 +0800

    1
    111

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+/* global module: true */
+module.exports = function (grunt) {
+    grunt.initConfig({
+        htmlmin:{
+            options:{
+                removeComments:true,
+                collapseWhitespace:true
+            },
+            files: {
+                src:'./index.html',
+                dest:'dist/index.html'
+            }
+        },
+        cssmin: {
+            'dist/index.css':'index.css'
+        },
+        uglify: {
+            files: {'dist/index.js':'index.js'}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['htmlmin','cssmin','uglify']);
+}
